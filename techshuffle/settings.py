d18ee9d86c3e748ef6363db9233450d4b23156f9
Abby Demarest <abbyjoydemarest@gmail.com>
2019-04-02 14:39:23 -0400

    changed timezone to America/New_York. updated middleware to include deubg_toolbar

--- techshuffle/settings.py
+++ techshuffle/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'techshuffle.urls'
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
