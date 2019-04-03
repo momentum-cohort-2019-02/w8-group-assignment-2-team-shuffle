cd68671f28f8e4da3eaaf479cc75e70cd958d4d0
Abby Demarest <abbyjoydemarest@gmail.com>
2019-04-03 16:30:51 -0400

    fixed a misspelling in settings.py to make able to deploy to heroku

--- techshuffle/settings.py
+++ techshuffle/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.joing(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Quick-start development settings - unsuitable for production
