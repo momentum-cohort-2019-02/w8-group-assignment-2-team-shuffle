51445ea5e19140c6b0a3f5f144b02c0b9245b234
Abby Demarest <abbyjoydemarest@gmail.com>
2019-04-03 16:12:20 -0400

    updated settings to import heroku

--- techshuffle/settings.py
+++ techshuffle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #debug
 INTERNAL_IPS ='127.0.0.1'
+
+#heroku
+django_heroku.settings(locals())
